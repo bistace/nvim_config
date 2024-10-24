e20c8dfef52df27d17f6cab3976b2dcf7ed4b8ad
Benjamin Istace <benjamin.istace@gmail.com>
2024-10-24 18:00:55 +0200

    Add LSP completions (Credits: typecraft.dev)

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,12 +1,14 @@
 return {
   {
     "williamboman/mason.nvim",
+    lazy = false,
     config = function()
       require("mason").setup()
     end
   },
   {
     "williamboman/mason-lspconfig.nvim",
+    lazy = false,
     config = function()
       require("mason-lspconfig").setup({
         ensure_installed = {
@@ -21,16 +23,21 @@ return {
   -- Global LSP config
   {
     "neovim/nvim-lspconfig",
+    lazy = false,
     config = function()
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
       vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
 
       lspconfig.als.setup({
+        capabilities = capabilities,
         on_init = require("gpr_selector").als_on_init,
       })
     end
