e0ac327b3f51be7bfcba339ef60bb63eb16ba6a3
Benjamin Istace <benjamin.istace@gmail.com>
2024-10-25 13:35:36 +0200

    Add hardtime.nvim

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,13 @@
+return {
+	"m4xshen/hardtime.nvim",
+	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+	opts = {
+		showmode = false,
+		disabled_keys = {
+			["<Up>"] = {},
+			["<Down>"] = {},
+			["<Left>"] = {},
+			["<Right>"] = {},
+		},
+	},
+}
