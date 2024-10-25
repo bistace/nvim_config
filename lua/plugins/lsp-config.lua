dbd412b607e9bda233b478c0d80ca4b9758b9e39
Benjamin Istace <benjamin.istace@gmail.com>
2024-10-25 08:38:46 +0200

    Add Rust LSP

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,45 +1,51 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls"
-        }
-      })
-    end
-  },
-  -- Ada
-  "TamaMcGlinn/nvim-lsp-gpr-selector",
-  "TamaMcGlinn/nvim-lspconfig-ada",
-  -- Global LSP config
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
-      vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+				},
+			})
+		end,
+	},
+	-- Ada
+	"TamaMcGlinn/nvim-lsp-gpr-selector",
+	"TamaMcGlinn/nvim-lspconfig-ada",
+	-- Rust
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^5",
+		lazy = false,
+	},
+	-- Global LSP config
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
 
-      lspconfig.als.setup({
-        capabilities = capabilities,
-        on_init = require("gpr_selector").als_on_init,
-      })
-    end
-  },
+			lspconfig.als.setup({
+				capabilities = capabilities,
+				on_init = require("gpr_selector").als_on_init,
+			})
+		end,
+	},
 }
