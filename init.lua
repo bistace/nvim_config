67d2db118d78948787c432d64b7c9faa3fcd5d2c
Benjamin Istace <benjamin.istace@gmail.com>
2024-10-25 13:36:37 +0200

    Add line numbers

--- init.lua
+++ init.lua
@@ -4,6 +4,9 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.cmd.colorscheme "brogrammer"
+vim.opt.number = true
+vim.opt.relativenumber = true
+
 
 -- Lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
