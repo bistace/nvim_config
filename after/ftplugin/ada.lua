976f8ef380762384af0c57de3de874799db78dea
Benjamin Istace <benjamin.istace@gmail.com>
2024-10-26 09:43:41 +0200

    Remove ada keybinds that introduce input lag

--- after/ftplugin/ada.lua
+++ after/ftplugin/ada.lua
@@ -1,4 +1,4 @@
 -- Remove these keybinds as they introduce input lag
 local bufnr = vim.api.nvim_get_current_buf()
-vim.keymap.set("n", "<leader>aj", function() end, { silent = true, buffer = bufnr })
-vim.keymap.set("n", "<leader>al", function() end, { silent = true, buffer = bufnr })
+vim.keymap.del("i", "<leader>aj", { buffer = bufnr })
+vim.keymap.del("i", "<leader>al", { buffer = bufnr })
